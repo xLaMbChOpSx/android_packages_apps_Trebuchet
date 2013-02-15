405b0053cf54b5b090b9f0ad986240c49890bef6
Steve Kondik <shade@chemlab.org>
2013-02-15 14:57:09 -0800

    trebuchet: Avoid NPE on startup
    The workspaces are now inflated before the LauncherView, so defer
    wallpaper initialization until later.
    
    Change-Id: I8b939ec217e73b4a7c2b14434479982e24315c74

--- src/com/cyanogenmod/trebuchet/Launcher.java
+++ src/com/cyanogenmod/trebuchet/Launcher.java
@@ -2554,8 +2554,10 @@ public final class Launcher extends Activity
     }
 
     private void setWorkspaceBackground(boolean workspace) {
-        mLauncherView.setBackground(workspace ?
-                mWorkspaceBackgroundDrawable : mBlackBackgroundDrawable);
+        if (mLauncherView != null) {
+            mLauncherView.setBackground(workspace ?
+                    mWorkspaceBackgroundDrawable : mBlackBackgroundDrawable);
+        }
     }
 
     void updateWallpaperVisibility(boolean visible) {
